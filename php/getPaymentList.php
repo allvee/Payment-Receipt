e2a1b32cf5765a70f1f712d1e19484966a007764
allvee <mdalamincuet@gmail.com>
2017-01-02 16:15:11 +0600

    2 php added

---
+++ php/getPaymentList.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anik-pc
+ * Date: 1/2/2017
+ * Time: 12:01 AM
+ */
+
+
+include_once "config.php";
+$isError = 0;
+
+$qry = "SELECT Payment_ID,Payment_Name FROM payment_list";
+
+try{
+    $res = Sql_exec($con,$qry);
+}catch (Exception $e){
+    $isError =1;
+}
+$i=0;
+$data = [];
+while($dt = Sql_fetch_assoc($res)){
+    $data[$i++] = '<option value="'.$dt['Payment_ID'].'">'.$dt['Payment_Name'].'</option>';
+}
+echo json_encode($data);
---
+++ php/getReceiptList.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anik-pc
+ * Date: 1/2/2017
+ * Time: 12:02 AM
+ */
+
+include_once "config.php";
+$isError = 0;
+
+$qry = "SELECT Receipt_ID,Receipt_Name FROM receipt_list";
+
+try{
+    $res = Sql_exec($con,$qry);
+}catch (Exception $e){
+    $isError =1;
+}
+$i=0;
+$data = [];
+while($dt = Sql_fetch_assoc($res)){
+    $data[$i++] = '<option value="'.$dt['Receipt_ID'].'">'.$dt['Receipt_Name'].'</option>';
+}
+echo json_encode($data);
